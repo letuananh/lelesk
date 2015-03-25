cd6821ccf8a56c9c234f2fb2e827834210d11ebf
Le Tuan Anh <tuananh.ke@gmail.com>
2015-03-25 14:39:30 +0800

    Code refactoring - half way

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 
-python main.py semcor semcor_wn30.txt > semcor_wn30.out.txt
+python main.py semcor data/semcor_wn30.txt > data/semcor_wn30.out.txt
 #python main.py semcor semcor_wn30.txt
