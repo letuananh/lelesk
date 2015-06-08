28b6f61d6b0231947fc108e0137901a5735e73fe
Le Tuan Anh <tuananh.ke@gmail.com>
2015-06-08 21:21:46 +0800

    filename error

--- speckled_all.sh
+++ speckled_all.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/sh
 
-python3 wsdtk.py -b 'data/specdev.txt' -o 'data/specdev_ll_summary.txt' > 'data/specdev_ll_details.txt'
-python3 wsdtk.py -b 'data/specdev.txt' -o 'data/specdev_mfs_summary.txt' -m mfs > 'data/specdev_mfs_details.txt'
+python3 wsdtk.py -b 'data/specall.txt' -o 'data/specall_ll_summary.txt' > 'data/specall_ll_details.txt'
+python3 wsdtk.py -b 'data/specall.txt' -o 'data/specall_mfs_summary.txt' -m mfs > 'data/specall_mfs_details.txt'
 
 
 echo '-------------------------------------------------------'
 echo 'LELESK+MFS'
-tail -n 7 data/specdev_ll_details.txt
+tail -n 7 data/specall_ll_details.txt
 echo '-------------------------------------------------------'
 echo 'MFS'
-tail -n 7 data/specdev_mfs_details.txt
+tail -n 7 data/specall_mfs_details.txt
 
