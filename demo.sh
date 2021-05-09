3e472c8687a09bb0b36ac3a3fd7a58f4530e219f
Le Tuan Anh <tuananh.ke@gmail.com>
2021-05-09 17:57:03 +0800

    update demo script

--- demo.sh
+++ demo.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-python3 -m lelesk.wsdtk -W "love" -x "I love cooking." -r ~/tmp/lelesk-wsd-i-love-cooking.txt
-python3 -m lelesk.wsdtk -b data/test.txt -o data/test_report.txt -r data/test_debug.txt
+# Disambiguate a sentence
+python3 -m lelesk wsd "I go to the bank to get money."
+
+# Disambiguate a text file and write output to demo_wsd.json in TTL/JSON format
+python3 -m lelesk file data/demo.txt data/demo_wsd.json --ttl json
